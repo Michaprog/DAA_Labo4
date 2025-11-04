1437197126198b2f53ecc692b0ff2f810c40c057
Michaprog <vejnstejn2@gmail.com>
2025-11-04 17:07:47 +0100

    setup

--- models/State.kt
+++
@@ -1,5 +0,0 @@
-package ch.heigvd.iict.daa.template.models
-
-enum class State {
-    IN_PROGRESS, DONE
-}
--- models/Type.kt
+++
@@ -1,5 +0,0 @@
-package ch.heigvd.iict.daa.template.models
-
-enum class Type {
-    NONE, TODO, SHOPPING, WORK, FAMILY
-}
